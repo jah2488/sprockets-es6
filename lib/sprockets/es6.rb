07ecce2804636f632ccc312db518f900c7edb912
Justin Herrick <justin@justinherrick.com>
2016-03-05 17:30:09 -0600

    Moving register_engine to a configuration block.
    Due to updates in sprockets and in rails, the configuration block needs to be used. https://github.com/rails/sprockets-rails/issues/292#issuecomment-165894932

--- lib/sprockets/es6.rb
+++ lib/sprockets/es6.rb
@@ -87,5 +87,8 @@ module Sprockets
   register_mime_type 'text/ecmascript-6', extensions: ['.es6'], charset: :unicode
   register_transformer 'text/ecmascript-6', 'application/javascript', ES6
   register_preprocessor 'text/ecmascript-6', DirectiveProcessor
-  register_engine '.es6', ES6
+  
+  Rails.application.config.assets.configure do |env|
+    env.register_engine '.es6', ES6
+  end
 end
